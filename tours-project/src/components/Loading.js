dc9ef31a8da9dfd2da5d313bf13d217904b00da4
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2021-04-10 13:32:59 +0300

    Add Tours project

---
+++ tours-project/src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div>
+            <div className="loading">
+                <h1>loading...</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Loading

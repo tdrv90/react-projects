6e42f0984181e0ea0b6a4e528576eda4e098c8eb
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2021-04-11 22:22:24 +0300

    Initial commit for Lorem Ipsum Generator

---
+++ lorem-ipsum-generator/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import './index.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hello world');
+  }
+
+  return (
+    <section className="section-center">
+      <h3>tired ot the boring lorem ipsum?</h3>
+      <form
+        className="lorem-form"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="amount">paragraphs: </label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)} />
+        <button type="submit" className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return (
+            <p key={index}>{item}</p>
+          )
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
